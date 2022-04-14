85269f02d3040b71c6cbfbf5f7b09d360b38bed0
eunji <dmswl0489@gmail.com>
2022-04-14 22:51:40 +0900

    implementation_example_01

---
+++ implementation01.cpp
@@ -0,0 +1,40 @@
+// page 110 »óÇÏÁÂ¿ì
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int N;
+	string moves; // save route
+
+	// read input
+	cin >> N;
+	cin.ignore();
+	getline(cin, moves);
+
+	// coordinate. start at (1,1)
+	int x = 1;
+	int y = 1; 
+
+	// start moving
+	for (int i = 0; i < moves.length(); i++) {
+		if (moves[i] == 'L' && y > 1) {
+			y--;
+		}
+		else if (moves[i] == 'R' && y < N) {
+			y++;
+		}
+		else if (moves[i] == 'U' && x > 1) {
+			x--;
+		}
+		else if (moves[i] == 'D' && x < N) {
+			x++;
+		}
+	}
+
+	cout << x << " " << y;
+
+	return 0;
+}
