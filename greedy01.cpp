a9a05af2c9fa4962851b34a41156f4966f88403f
eunji <dmswl0489@gmail.com>
2022-04-14 16:19:12 +0900

    greedy_example_01

---
+++ greedy01.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, k;
+	cin >> n >> m >> k;
+
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	// sorting
+	for (int i = 0; i < n-1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (arr[i] > arr[j]) {
+				swap(arr[i], arr[j]);
+			}
+		}
+	}
+
+	int first = arr[n - 1]; // biggest number
+	int second = arr[n - 2]; // second big number
+
+	int repeatN = m / (k + 1); // repeat number
+	int remain = m % (k + 1); 
+
+	int result;
+
+	result = (first * k + second) * repeatN + remain * first;
+	cout << result;
+}
